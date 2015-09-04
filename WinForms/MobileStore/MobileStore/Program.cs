21e8874ef8b039a6dacfc81b0c97f486a9c4ac15
Pavel Chehov <good-1991@mail.ru>
2015-09-05 01:09:05 +0300

    reformat code in MobileStore

--- WinForms/MobileStore/MobileStore/Program.cs
+++ WinForms/MobileStore/MobileStore/Program.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MobileStore
-{
-   static class Program
-   {
+namespace MobileStore {
+   internal static class Program {
       /// <summary>
       /// The main entry point for the application.
       /// </summary>
       [STAThread]
-      static void Main()
-      {
+      private static void Main() {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new Form1());
