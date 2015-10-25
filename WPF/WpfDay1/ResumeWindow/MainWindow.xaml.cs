12cb55cce49de42e9b17e7ad3bad9dede5ca0454
Pavel Chehov <good-1991@mail.ru>
2015-10-25 12:33:27 +0300

    add wpf day1

---
+++ WPF/WpfDay1/ResumeWindow/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResumeWindow {
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window {
+      public MainWindow() {
+         InitializeComponent();
+      }
+
+      private void ButtonOk_OnClick(object sender, RoutedEventArgs e) {
+         EmployeeType eEmployeeType = EmployeeType.SysAdm;
+         List<Skills> objSkills = new List<Skills>();
+         if(RadioButton1.IsChecked == true) {
+            eEmployeeType = EmployeeType.SysAdm;
+         }
+         if(RadioButton2.IsChecked == true) {
+            eEmployeeType = EmployeeType.Programmer;
+         }
+         if(RadioButton3.IsChecked == true) {
+            eEmployeeType = EmployeeType.ProductManager;
+         }
+
+         if(CheckBox1.IsChecked == true) {
+            objSkills.Add(Skills.CSharp);
+         }
+         if(CheckBox2.IsChecked == true) {
+            objSkills.Add(Skills.CPlusPlus);
+         }
+         if(CheckBox3.IsChecked == true) {
+            objSkills.Add(Skills.Linux);
+         }
+
+         var objInfo = new ResumeInfo() {
+            FirstName = TextBox1.Text,
+            LastName = TextBox2.Text,
+            BirthDate = TextBox3.Text,
+            EmployeeType = eEmployeeType,
+            Skills = objSkills
+         };
+
+
+         using(var objWriter = new StreamWriter("list.txt")) {
+            objWriter.Write(objInfo.ToString());
+         }
+      }
+
+      private void ButtonCancel_OnClick(object sender, RoutedEventArgs e) {
+         this.Close();
+      }
+   }
+}
---
+++ WPF/WpfDay1/ResumeWindow/ResumeInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using ResumeWindow;
+
+namespace ResumeWindow {
+   public enum Skills {
+      CSharp,
+      CPlusPlus,
+      Linux
+   }
+
+   public enum EmployeeType {
+      SysAdm,
+      Programmer,
+      ProductManager
+   }
+
+   public class ResumeInfo {
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string BirthDate { get; set; }
+      public List<Skills> Skills { get; set; }
+      public EmployeeType EmployeeType { get; set; }
+
+      public override string ToString() {
+         string strInfo = String.Format("FirstName: {0}\nLastName: {1}\nBirthDate: {2}\n", FirstName, LastName, BirthDate);
+         strInfo += "Specialization: ";
+         switch(EmployeeType) {
+         case EmployeeType.SysAdm:
+            strInfo += "SystemAdministrator\n";
+            break;
+         case EmployeeType.Programmer:
+            strInfo += "Programmer\n";
+            break;
+         case EmployeeType.ProductManager:
+            strInfo += "ProductManager\n";
+            break;
+         default:
+            break;
+         }
+         strInfo += "Skills: ";
+         foreach(Skills skill in Skills) {
+            switch(skill) {
+            case ResumeWindow.Skills.CPlusPlus:
+               strInfo += "C++\n";
+               break;
+            case ResumeWindow.Skills.CSharp:
+               strInfo += "C#\n";
+               break;
+            case ResumeWindow.Skills.Linux:
+               strInfo = "Linux\n";
+               break;
+            }
+         }
+         return strInfo;
+      }
+   }
+}
