0fe944691aabcb138fa0781b398e74a31e53e530
feliasfogg <good-1991@mail.ru>
2016-04-30 22:59:34 +0300

    no message

---
+++ ASP/WebAjaxApp/WebAjaxApp/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAjaxApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title></title>
+</head>
+<body>
+<form id="form1" runat="server">
+   <asp:ScriptManager ID="ScriptManager1" runat="server">
+   </asp:ScriptManager>
+   <div>
+
+      <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+      <asp:UpdatePanel ID="UpdatePanel1" runat="server" >
+         <ContentTemplate>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+         </ContentTemplate>
+         <Triggers>
+            <asp:AsyncPostBackTrigger ControlID="DropDownList1" EventName="SelectedIndexChanged" />
+         </Triggers>
+      </asp:UpdatePanel>
+
+   </div>
+
+</form>
+</body>
+</html>
---
+++ ASP/WebAjaxApp/WebAjaxApp/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAjaxApp {
+   public partial class WebForm1 : System.Web.UI.Page {
+      private Dictionary<string, List<string>> _Country = new Dictionary<string, List<string>>();
+      private List<string> _Cities = new List<string>();
+
+      protected void Page_Load(object sender, EventArgs e) {
+      }
+
+      protected void Button1_Click(object sender, EventArgs e) {
+         //Label1.Text = TextBox1.Text;
+      }
+
+      protected override void OnInit(EventArgs e) {
+         _Cities.Add("Минск");
+         _Cities.Add("Гомель");
+         _Country.Add("Беларусь", _Cities);
+         _Country.Add("Лаос", _Cities);
+
+         DropDownList1.DataSource = _Country.Keys;
+         DropDownList1.DataBind();
+
+      }
+
+      protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e) {
+         DropDownList2.DataSource = _Country[DropDownList2.SelectedItem.ToString()];
+         DropDownList2.DataBind();
+      }
+   }
+}
